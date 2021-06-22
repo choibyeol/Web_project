5224d26c6022119f0afe032eadfee5522188a5e3
ChoiByeol <honey333888@naver.com>
2021-06-22 17:51:15 +0900

    Date 입력 포맷 추가

--- calendar_board/forms.py
+++ calendar_board/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import BoardList
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class BoardForm(forms.ModelForm):
     class Meta:
         model = BoardList
         fields = ('title', 'content', 'end_date')
+        widgets = {
+            'end_date' : DateInput()
+        }
